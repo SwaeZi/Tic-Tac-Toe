fd0e6bab3233b34b8b074cd9196e9c24f5a2bf9c
SwaeZi <123456789+odin@users.noreply.github.com>
2023-05-05 11:16:45 +0200

    add player X

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Tic-Tac-Toe
 
-const gameBoard = {
-    board: [],
-    player0: [player1, player2],
+const buttons = document.querySelectorAll(".btnoption");
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "O";
 
-}
+for (let i = 0; i < buttons.length; i++) {
+    let btn = buttons[i];
+    btn.addEventListener("click", function() {
+        main(btn);
+    });
+};
+
+function main(btn) {
+    btn.innerHTML = currentPlayer;
+};
+ 
+function changePlayer(btn) {
+    if (currentPlayer == "O") {
+        currentPlayer == "X";
+    } else {
+        currentPlayer = "O";
+    };
+};
